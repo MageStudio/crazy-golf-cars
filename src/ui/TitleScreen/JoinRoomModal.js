1031f86cb6612096688f865d5d942b18cce14211
Marco Stagni <m.stagni@elsevier.com>
2021-11-01 22:07:01 +0000

    showing rooms

--- src/ui/TitleScreen/JoinRoomModal.js
+++ src/ui/TitleScreen/JoinRoomModal.js
@@ -1,10 +1,18 @@
-import { useState } from 'xferno';
+import { useState, useEffect } from 'xferno';
+import { getRoomsEndpoint } from '../../network/constants';
 import { EnterIcon } from '../icons';
 import Modal from '../lib/Modal';
 import RoomsList from './RoomsList';
 
 const JoinRoomModal = ({ onConfirm, onCancel, ...rest }) => {
     const [roomName, setRoomName] = useState('');
+    const [roomsList, setRooms] = useState([]);
+
+    useEffect(() => {
+        fetch(getRoomsEndpoint())
+            .then(res => res.json())
+            .then((json = {}) => setRooms(Object.keys(json)));
+    });
 
     const onInputChange = event => {
         const { value } = event.target;
@@ -26,7 +34,9 @@ const JoinRoomModal = ({ onConfirm, onCancel, ...rest }) => {
             {...rest}
             dismissable
             onConfirm={handleModalConfirm}>
-            <RoomsList/>
+            <RoomsList
+                rooms={roomsList}
+                onRoomSelection={onConfirm}/>
             <div className='input-container'>
                 <EnterIcon/>
                 <input
--- src/ui/TitleScreen/RoomsList.js
+++ src/ui/TitleScreen/RoomsList.js
@@ -1,32 +1,26 @@
-const RoomsList = () => (
-    <ul class='unordered-list'>
-        <h2 class='title'>Available Rooms:</h2>
-        <div class='break'></div>
-        <li class='item'>
-            <span class='label'>Room 1</span>
-            <button class='primary small button'>
-                JOIN
-            </button>
-        </li>
-        <li class='item'>
-            <span class='label'>Room 1</span>
-            <button class='primary small button'>
-                JOIN
-            </button>
-        </li>
-        <li class='item'>
-            <span class='label'>Room 1</span>
-            <button class='primary small button'>
-                JOIN
-            </button>
-        </li>
-        <li class='item'>
-            <span class='label'>Room 1</span>
-            <button class='primary small button'>
-                JOIN
-            </button>
-        </li>
-  </ul>
-);
+const RoomsList = ({ rooms = [], onRoomSelection }) => {
+    const mapRooms = () => (
+        rooms.map(room => (
+            <li class='item'>
+                <span class='label'>{ room }</span>
+                <button
+                    onClick={() => onRoomSelection(room)}
+                    class='primary small button'>
+                    JOIN
+                </button>
+            </li>
+        ))
+    );
+
+    const getEmptyListMessage = () => <span>No roms available</span>;
+
+    return (
+        <ul class='unordered-list'>
+            <h2 class='title'>Available Rooms:</h2>
+            <div class='break'></div>
+            { rooms.length ? mapRooms() : getEmptyListMessage() }
+        </ul>
+    )
+}
 
 export default RoomsList;
